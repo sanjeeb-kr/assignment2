eac93c29437b1cf2fa2b0da061a5715b39d4654c
Sanjeeb8240 <kumarsanjeeb017@gmail.com>
2026-04-18 21:19:14 +0530

    final working structure

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "CI/CD + Docker + EC2 deployment working!"
+    return "CI/CD + Docker + EC2 working!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
